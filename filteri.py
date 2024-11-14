752728d9f2ccb55863b69957d8077ba21b91ce56
ruleva <139155207+ruleva@users.noreply.github.com>
2024-11-15 00:10:48 +0100

    Update filteri.py

--- filteri.py
+++ filteri.py
@@ -1,118 +1,64 @@
 import time
-from concurrent.futures import ThreadPoolExecutor, as_completed
-
-# from telnetlib import EC
-
+import requests
 import pandas as pd
-from selenium import webdriver
-from selenium.common import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
-from datetime import datetime
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from bs4 import BeautifulSoup
+from datetime import datetime
 import os
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+BASE_URL = 'https://www.mse.mk/mk/stats/symbolhistory/'
+
 
 
 def filter1():
-    browser = webdriver.Chrome(options=options)
-    browser.get('https://www.mse.mk/mk/stats/symbolhistory/kmb')
-    dropdown_list = browser.find_element(By.CSS_SELECTOR, '#Code.form-control')
-    # print(dropdown_list)
-    select = Select(dropdown_list)
-    issuers1 = select.options
-    issuers = []
-    for issuer in issuers1:
-        flag = 0
-        for ch in issuer.text:
-            if ch.isdigit():
-                flag = 1
-        if flag == 0:
-            issuers.append(issuer)
+    url = 'https://www.mse.mk/mk/stats/symbolhistory/kmb'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    dropdown_list = soup.select('#Code.form-control option')
+    issuers = [option for option in dropdown_list if not any(char.isdigit() for char in option.text)]
     return issuers
 
 
 def filter2(issuer):
-    browser = webdriver.Chrome(options=options)
-    file_path = 'C:\\Users\\rulev\\PycharmProjects\\dians_prva_domasna\\' + issuer.text + '.csv'
+    file_path = f'C:\\Users\\rulev\\PycharmProjects\\dians_prva_domasna\\{issuer.text}.csv'
+
     if os.path.exists(file_path):
         data_loaded = pd.read_csv(file_path)
         date = data_loaded.iloc[0, 0]
         return date
     else:
-        url = 'https://www.mse.mk/mk/stats/symbolhistory/' + issuer.text.lower()
+        url = BASE_URL + issuer.text.lower()
         todays_date = datetime.today()
         day = todays_date.day + 1
         month = todays_date.month
-        browser.get(url)
+
         data = []
         for i in range(11):
             year = todays_date.year - i
-            datef = str(day) + '.' + str(month) + '.' + str(year)
-            datet = str(day - 1) + '.' + str(month) + '.' + str(year + 1)
-            data_row = collect_data(browser, datef, datet)
-            if data_row is not None:
+            datef = f"{day}.{month}.{year}"
+            datet = f"{day - 1}.{month}.{year + 1}"
+            data_row = collect_data(url, datef, datet)
+            if data_row:
                 data.extend(data_row)
 
         df = pd.DataFrame(data)
-        # print(df)
-        df.to_csv('C:\\Users\\rulev\\PycharmProjects\\dians_prva_domasna\\' + issuer.text + '.csv', index=False)
-        if not df.empty:
-            return df.iloc[0, 0]
-        else:
-            return None
+        df.to_csv(file_path, index=False)
+        return df.iloc[0, 0] if data else None
 
 
-def filter3(issuer, date):
-    browser = webdriver.Chrome(options=options)
-    url = 'https://www.mse.mk/mk/stats/symbolhistory/' + issuer.text.lower()
-    browser.get(url)
-    today = datetime.today().strftime('%d.%m.%Y')
-    if date != today and date is not None:
-        file_path = 'C:\\Users\\rulev\\PycharmProjects\\dians_prva_domasna\\' + issuer.text + '.csv'
-        data_loaded = pd.read_csv(file_path)
-        new_data = collect_data(browser, date, today)
-        if new_data is not None:
-            new_data_df = pd.DataFrame(new_data)
-            data = pd.concat((data_loaded, new_data_df), ignore_index=True)
-        data.to_csv(file_path, index=False)
+def collect_data(url, datef, datet):
+    payload = {
+        'FromDate': datef,
+        'ToDate': datet,
+    }
+    response = requests.post(url, data=payload)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-
-def collect_data(browser, datef, datet):
-    try:
-        WebDriverWait(browser, 4).until(
-            EC.presence_of_element_located((By.ID, 'FromDate'))
-        )
-    except TimeoutException:
-        return
-    from_date = browser.find_element(By.ID, 'FromDate')
-    from_date.clear()
-    from_date.send_keys(datef)
-    to_date = browser.find_element(By.ID, 'ToDate')
-    to_date.clear()
-    to_date.send_keys(datet)
-    show = browser.find_element(By.CSS_SELECTOR, '.container-end >input')
-    browser.execute_script('arguments[0].scrollIntoView();', show)
-    show.click()
-    # time.sleep(12)
-    try:
-        WebDriverWait(browser, 4).until(
-            EC.presence_of_element_located((By.ID, 'resultsTable'))
-        )
-    except TimeoutException:
-        return
-
-    soup = BeautifulSoup(browser.page_source, 'html.parser')
     table = soup.select_one('#resultsTable')
-
-    rows = table.select('tr')
+    rows = table.select('tr') if table else []
     year_rows = []
+
     for row in rows:
         cells = row.select('td')
         if cells and cells[7].text != '0':
@@ -130,6 +76,19 @@ def collect_data(browser, datef, datet):
     return year_rows
 
 
+def filter3(issuer, date):
+    url = BASE_URL + issuer.text.lower()
+    today = datetime.today().strftime('%d.%m.%Y')
+    if date != today and date is not None:
+        file_path = f'C:\\Users\\rulev\\PycharmProjects\\dians_prva_domasna\\{issuer.text}.csv'
+        data_loaded = pd.read_csv(file_path)
+        new_data = collect_data(url, date, today)
+        if new_data:
+            new_data_df = pd.DataFrame(new_data)
+            data = pd.concat((data_loaded, new_data_df), ignore_index=True)
+            data.to_csv(file_path, index=False)
+
+
 def process_issuer(issuer):
     date = filter2(issuer)
     if date:
@@ -141,19 +100,16 @@ def pipe():
     start_time = time.time()
 
     issuers = filter1()
-    with ThreadPoolExecutor(max_workers=10) as executor:
+
+    with ThreadPoolExecutor(max_workers=15) as executor:
         futures = [executor.submit(process_issuer, issuer) for issuer in issuers]
 
         for future in as_completed(futures):
             try:
-                issuer_text = future.result()  
+                issuer_text = future.result()
                 print(f"Completed processing for issuer: {issuer_text}")
             except Exception as e:
                 print(f"An error occurred: {e}")
-
-    # for issuer in issuers:
-    #    date = filter2(issuer)
-    #    filter3(issuer, date)
 
     end_time = time.time()
     elapsed_time = end_time - start_time
